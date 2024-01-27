42012d41997da62d8c0eed9d9f31aea4811bfad1
Tino Restivo <trestivo8498@conestogac.on.ca>
2024-01-27 11:10:35 -0500

    Added struct Task

--- taskmanager.c
+++ taskmanager.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+
+#define MAX_TITLE_LENGTH 40
+#define MAX_DESCRIPTION_LENGTH 200
+
+
+struct Task
+{
+    int taskId;
+    char title[MAX_TITLE_LENGTH + 1];
+    char description[MAX_DESCRIPTION_LENGTH + 1];
+    struct Task* nextTask;
+};
+
 int main(void)
 {
     printf("test");
