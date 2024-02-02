4771851c0cdcfaa1f2bc3c3ec20216b4a195f645
Tino Restivo <trestivo8498@conestogac.on.ca>
2024-02-02 15:29:31 -0500

    Reogranized code so it has seperate header and main file

---
+++ assignment1.c
@@ -0,0 +1,116 @@
+#include "taskmanager.h"
+
+
+int main(void)
+{
+	struct Task* head = NULL;
+
+	int option = 0;
+	int arrayOfIDs[MAX_TASKS_NUMBER] = { 0 };
+	int currentElement = 0;
+
+	while (option != 5)
+	{
+		printf("\nPress 1 to Add Task \n");
+		printf("Press 2 to Delete Task \n");
+		printf("Press 3 to Find Task \n");
+		printf("Press 4 to Print Tasks \n");
+		printf("Press 5 to Exit \n");
+		printf(">>>  ");
+
+		option = getNum();
+
+		if (option == 1)
+		{
+			char title[MAX_TITLE_LENGTH + 1] = { 0 };
+			char description[MAX_DESCRIPTION_LENGTH + 1] = { 0 };
+			int taskID = NO_VALID_DIGIT;
+
+			while (taskID < 0)
+			{
+				int inputId = 0;
+				printf("\nEnter digital ID for task being added\n");
+				printf(">>>  ");
+				inputId = getNum();
+
+				if (inputId == NO_VALID_DIGIT || inputId == 0)
+				{
+					printf("No valid ID was entered.\n");
+				}
+				else
+				{
+					int duplicated = checkForDuplicate(arrayOfIDs, &currentElement, inputId);
+					if (duplicated == DUPLICATE)
+					{
+						printf("\n ID %d already exists.\n", inputId);
+
+					}
+					else
+					{
+						taskID = inputId;
+					}
+				}
+			}
+			printf("\nEnter title for task being added\n");
+
+			printf(">>>  ");
+			fgets(title, MAX_TITLE_LENGTH, stdin);
+			validateInput(title);
+
+			printf("\nEnter description for task being added\n");
+			printf(">>>  ");
+
+			fgets(description, MAX_TITLE_LENGTH, stdin);
+			validateInput(description);
+
+			head = addTask(head, taskID, title, description);
+
+		}
+		else if (option == 2)
+		{
+			printf("\nEnter digital ID for task being deleted\n");
+			printf(">>>  ");
+			int taskToDelete = getNum();
+
+			if (taskToDelete == NO_VALID_DIGIT)
+			{
+				printf("No valid ID was entered.\n");
+			}
+			else
+			{
+				//Delete task function
+				int status = deleteTask(&head, taskToDelete);
+
+				if (status == 0)
+				{
+					printf("\nTask with Id %d was deleted\n", taskToDelete);
+				}
+				else
+				{
+					printf("\nUnable to find task with Id %d to perform deletion\n", taskToDelete);
+				}
+			}
+
+
+		}
+		else if (option == 3)
+		{
+			//Find tasks logic
+		}
+		else if (option == 4)
+		{
+			printTask(head);
+		}
+		else if (option == 5)
+		{
+			return 0;
+		}
+		else
+		{
+			printf("ERROR: Invalid input. Please type in a valid option.\n");
+
+		}
+	}
+
+	return 0;
+}
---
+++ taskmanager.h
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#pragma warning(disable: 4996) 
+
+//Constants:
+#define MAX_TITLE_LENGTH 40 
+#define MAX_DESCRIPTION_LENGTH 200
+#define ID_FOUND 0
+#define ID_NOT_FOUND_YET 1
+#define NO_VALID_DIGIT -1
+#define DUPLICATE -2
+#define MAX_TASKS_NUMBER 500
+
+//Data types:
+struct Task
+{
+	int taskId;//The task Id
+	char title[MAX_TITLE_LENGTH + 1];//Max length a title can be plus null terminator
+	char description[MAX_DESCRIPTION_LENGTH + 1];//Max length a description can be plus null terminator
+	struct Task* nextTask;//Pointer to the next task in linked list
+};
+
+//Prototypes
+int getNum(void);
+struct Task* addTask(struct Task* head, int inputTaskId, char inputTitle[], char inputDescription[]);
+int deleteTask(struct Task** head, int inputTaskId);
+struct Task* FindTaskByIndex(struct Task* head, int index);
+void validateInput(char* input);
+void printTask(struct Task* head);
+int checkForDuplicate(int array[], int* position, int input);
